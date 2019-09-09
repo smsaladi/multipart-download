547517c12592dd4464b72d5a1d47214dcda12876
Shyam Saladi <saladi@caltech.edu>
2019-09-09 04:27:20 -0700

    faster as a single command to cat

--- assemble_parts.sh
+++ assemble_parts.sh
@@ -3,5 +3,6 @@
 pre=$1
 parts=$2
 
-seq $2 | parallel -j1 -k "cat $pre/$pre.part.{}.out" > $pre.tar
+seq $parts | sed -e "s/^/${pre}\/${pre}.part./" -e 's/$/.out/' | parallel --max-args 100000 "cat {} > $pre.tar"
+
 
